b47ee3d553654a9e7e9643f602a84ddff0ea6072
zhangzw12319 <zhangzw12319@163.com>
2024-01-22 00:33:16 +0800

    Update dist_train_nusc.sh
    fix long waiting time of DDP with no errors response

--- scripts/dist_train_nusc.sh
+++ scripts/dist_train_nusc.sh
@@ -1,8 +1,7 @@
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 cd ..
-nohup \
-python -m torch.distributed.launch --nproc_per_node=4 --use_env train.py \
+nohup python -m torch.distributed.launch --nproc_per_node=4 --use_env train.py \
 -c configs/pa_po_nuscenes_trainval.yaml \
 -l nusc_20230730_4k2bs_a6000.log \
 > nohup_nusc_20230730_4k2bs_a6000.log 2>&1 &
